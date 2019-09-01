2afe9892aa59cc699bf19b9255805f465ee933dc
Kingsley Zhong <40475664+Generaleoley@users.noreply.github.com>
2019-08-31 19:12:34 -0600

    Update gupta.py

--- gupta/gupta.py
+++ gupta/gupta.py
@@ -12,5 +12,15 @@ class Gupta(commands.Cog):
         self.bot = bot
     
     @commands.command(aliases=['ripgupta'])
-    async def ripgupta(self, ctx, count=int):
+    async def ripgupta(self, ctx, count, *, message):
+        """Put in a sizeable number and put a message afterward"""
+        int(count)
+        gupta = 468209010978455552
+        channel = 617525238392946699
+        mloop = 0
+        
+        while mloop > count:
+            await channel.send("{} {}".format(gupta.mention, message))
+            int(mloop)
+            mloop = mloop + 1
     
